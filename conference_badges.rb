cadad559b54823bca247837e3f9fbbf8a421046a
Ty Raino <mstymetris@yahoo.com>
2020-03-10 12:37:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -34,4 +34,12 @@ rooms
 end
 
 def printer(speakers)
+badge_messages = batch_badge_creator(attendees)
+badge_messages.each do |message|
+  puts message
+end
+room_messages = assign_rooms(room_assignments)
+room_messages.each do |message|
+  puts message
+end
 end
