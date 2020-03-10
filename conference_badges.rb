5f1f56d15661f50ea55657ea2d5b57a40e61c0fc
Ty Raino <mstymetris@yahoo.com>
2020-03-10 12:08:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+## color = ["red", "blue", "green"]
+
+## color.each_with_index do |colors, index|
+## puts "#{index}: #{colors}"
+## end
+
+## 0: red <=
+## 1: blue <=
+## 2: green <=
+
+
+
+
+def badge_maker(names)
+ "Hello, my name is #{names}."
+end
+
+def batch_badge_creator(attendees)
+  new_array = []
+  attendees.each do |attendee|
+    new_array << "Hello, my name is #{attendee}."
+  end
+  new_array
+end
+
+def assign_rooms(room)
+end
